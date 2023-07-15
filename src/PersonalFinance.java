e3da54419a7f3585a5b7f05ef985e6005d01e436
ArturD <artur2134@mail.ru>
2023-07-15 10:28:57 +0300

    Added text constants

---
+++ src/PersonalFinance.java
@@ -0,0 +1,24 @@
+import settings.Text;
+
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+
+public class PersonalFinance {
+    public static void main(String[] args) {
+        init();
+        System.out.println(Text.get("PROGRAM_NAME"));
+        System.out.println(Arrays.toString(Text.getMonths()));
+    }
+
+    private static void init() {
+        Text.init();
+        GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+        try {
+            ge.registerFont(Font.createFont(Font.TRUETYPE_FONT, new File("fonts/mon-amour-one-medium.ttf")));
+        } catch (FontFormatException | IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ src/settings/Text.java
@@ -0,0 +1,52 @@
+package settings;
+
+import java.util.HashMap;
+
+final public class Text {
+    private static final HashMap<String, String> data = new HashMap<>();
+
+    public static String get(String key) {
+        return data.get(key);
+    }
+
+    public static String[] getMonths() {
+        String[] months = new String[12];
+
+        months[0] = data.get("JANUARY");
+        months[1] = data.get("FEBRUARY");
+        months[2] = data.get("MARCH");
+        months[3] = data.get("APRIL");
+        months[4] = data.get("MAY");
+        months[5] = data.get("JUNE");
+        months[6] = data.get("JULY");
+        months[7] = data.get("AUGUST");
+        months[8] = data.get("SEPTEMBER");
+        months[9] = data.get("OCTOBER");
+        months[10] = data.get("NOVEMBER");
+        months[11] = data.get("DECEMBER");
+
+        return months;
+    }
+
+    public static void init() {
+        data.put("PROGRAM_NAME", "Домашняя бухгалтерия");
+        data.put("MENU_FILE", "Файл");
+        data.put("MENU_EDIT", "Правка");
+        data.put("MENU_VIEW", "Вид");
+        data.put("MENU_HELP", "Помощь");
+
+        data.put("JANUARY", "Январь");
+        data.put("FEBRUARY", "Февраль");
+        data.put("MARCH", "Март");
+        data.put("APRIL", "Апрель");
+        data.put("MAY", "Май");
+        data.put("JUNE", "Июнь");
+        data.put("JULY", "Июль");
+        data.put("AUGUST", "Август");
+        data.put("SEPTEMBER", "Сентябрь");
+        data.put("OCTOBER", "Октябрь");
+        data.put("NOVEMBER", "Ноябрь");
+        data.put("DECEMBER", "Декабрь");
+
+    }
+}
