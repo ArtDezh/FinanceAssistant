95e118ee535cc174d58921ae85706163acc87110
ArturD <artur2134@mail.ru>
2023-07-17 21:03:41 +0300

    Created main settings calss

--- src/PersonalFinance.java
+++ src/PersonalFinance.java
@@ -1,22 +1,23 @@
+import settings.Settings;
 import settings.Text;
 
 import java.awt.*;
-import java.io.File;
 import java.io.IOException;
-import java.util.Arrays;
 
 public class PersonalFinance {
     public static void main(String[] args) {
         init();
-        System.out.println(Text.get("PROGRAM_NAME"));
-        System.out.println(Arrays.toString(Text.getMonths()));
+
+//        System.out.println(Text.get("PROGRAM_NAME"));
+//        System.out.println(Arrays.toString(Text.getMonths()));
     }
 
     private static void init() {
+        Settings.init();
         Text.init();
         GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
         try {
-            ge.registerFont(Font.createFont(Font.TRUETYPE_FONT, new File("fonts/mon-amour-one-medium.ttf")));
+            ge.registerFont(Font.createFont(Font.TRUETYPE_FONT, Settings.FONT_MOM_AMOUR_ONE_MEDIUM));
         } catch (FontFormatException | IOException e) {
             throw new RuntimeException(e);
         }
